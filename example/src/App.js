189d2fe145cbb75afae9bc07aafabc1d0ccbbc5e
chaim <chaim.triebitz@bhbsdventures.com>
2024-01-04 17:16:16 +0200

    sec

--- example/src/App.js
+++ example/src/App.js
@@ -1,10 +1,10 @@
 import React from 'react'
 
-import { ExampleComponent } from 'ct-loader'
+import CtLoader from 'ct-loader'
 import 'ct-loader/dist/index.css'
 
 const App = () => {
-  return <ExampleComponent text="Create React Library Example 😄" />
+   return  <CtLoader />
 }
 
 export default App
